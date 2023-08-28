c5bd8c28d55840850b210d2a15193fcc5a44c3ac
Rida Ennasry <rida.ennasry@gmail.com>
2023-08-28 21:47:18 +0100

    init ex01

---
+++ day02/ex01/CosineSimilarity.java
@@ -0,0 +1,24 @@
+
+package day02.ex01;
+
+import java.io.BufferedReader;
+
+import java.util.Vector;
+
+class CosineSimilariy {
+    private double cosSim = 0;
+
+
+
+    public double getCosSim() {
+        return cosSim;
+    }
+
+
+    // Vector<Integer> getDocFrequency(BufferedReader buff) {
+
+    // }
+
+
+}
+
--- day02/ex01/Program.java
+++ day02/ex01/Program.java
@@ -1,2 +1,31 @@
 
 package day02.ex01;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+
+class Program {
+    public void main(String[] args) {
+
+        if (args.length < 2) {
+            // usage file1, file1
+            System.err.println("usage: java Program file1 file2");
+            System.exit(-1);
+        }
+        // FileWriter
+        // FileReader
+        BufferedReader bufferedReader = null;
+        BufferedReader bufferedReader2 = null;
+        try {
+         bufferedReader = new BufferedReader(new FileReader(args[0]));
+         bufferedReader2 = new BufferedReader(new FileReader(args[1]));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        
+        }        
+
+    }
+}
