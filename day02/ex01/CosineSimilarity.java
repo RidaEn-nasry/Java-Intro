692a68b82015ef5f2ec23e857c3b10eb3dce6661
RidaEn-nasry <rida.ennasry@gmail.com>
2023-08-29 18:04:25 +0100

    day02/ex01

--- day02/ex01/CosineSimilarity.java
+++ day02/ex01/CosineSimilarity.java
@@ -2,23 +2,122 @@
 package day02.ex01;
 
 import java.io.BufferedReader;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.TreeSet;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Vector;
 
 class CosineSimilariy {
-    private double cosSim = 0;
+    // vec of our word frequencies
+    private Vector<Hashtable<String, Integer>> wordFreq;
+    private BufferedWriter out;
+    // our dictionary
+    private Set<String> dict;
 
+    public CosineSimilariy() {
+        wordFreq = new Vector<Hashtable<String, Integer>>();
+        dict = new TreeSet<String>();
+        try {
+            out = new BufferedWriter(new FileWriter("dictionary.txt"));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    };
 
-
-    public double getCosSim() {
-        return cosSim;
+    // construct with one file
+    public CosineSimilariy(String filePath1, String filePath2) {
+        this();
+        this.addDocument(filePath1);
+        this.addDocument(filePath2);
     }
 
+    public void writeToDoc(String word) {
+        try {
+            out.write(word + "\n");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 
-    // Vector<Integer> getDocFrequency(BufferedReader buff) {
+    // add a doc to the corpus
+    public void addDocument(String filePath) {
+        if (wordFreq.size() >= 2) {
+            System.out.println("Only 2 documents are allowed");
+            return;
+        }
 
-    // }
+        // if file size > 10MB, don't add it
+        File file = new File(filePath);
+        if (file.length() > 10000000) {
+            System.out.println("File size is too big");
+            return;
+        }
+        if (file.length() == 0) {
+            wordFreq.add(null);
+        }
 
+        Hashtable<String, Integer> words = new Hashtable<String, Integer>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] tokens = line.split(" ");
+                for (String token : tokens) {
+                    dict.add(token);
+                    writeToDoc(token);
+                    if (words.get(token) == null) {
+                        words.put(token, 1);
+                    } else {
+                        words.put(token, words.get(token) + 1);
+                    }
+                }
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            System.out.println("Error reading file: " + filePath);
+            System.err.println(e.getMessage());
+        }
+        wordFreq.add(words);
+    }
+
+    // calcuate the cosine similarity between two specified documents
+    public double calcuateCosSim() {
+        // one of files was empty, similarity is 0
+        if (wordFreq.get(0) == null || wordFreq.get(1) == null) {
+            return 0;
+        }
+        Hashtable<String, Integer> words1 = wordFreq.get(0);
+        Hashtable<String, Integer> words2 = wordFreq.get(1);
+        if (words1 == null || words2 == null) {
+            System.out.println("Document not found");
+            return -1;
+        }
+        double dotProduct = 0;
+        double norm1 = 0;
+        double norm2 = 0;
+        try {
+            for (String word : dict) {
+                int occr1 = words1.get(word) == null ? 0 : words1.get(word);
+                int occr2 = words2.get(word) == null ? 0 : words2.get(word);
+                dotProduct += occr1 * occr2;
+                norm1 += words1.get(word) == null ? 0 : words1.get(word) * words1.get(word);
+                norm2 += words2.get(word) == null ? 0 : words2.get(word) * words2.get(word);
+            }
+            return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
+        } catch (Exception e) {
+            System.out.println("Error calculating cosine similarity");
+            return -1;
+        } finally {
+            try {
+                out.close();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
 
 }
-
--- day02/ex01/Program.java
+++ day02/ex01/Program.java
@@ -1,31 +1,14 @@
 
 package day02.ex01;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.Reader;
-
 class Program {
-    public void main(String[] args) {
-
-        if (args.length < 2) {
-            // usage file1, file1
-            System.err.println("usage: java Program file1 file2");
+    public static void main(String[] args) {
+        if (args.length < 2 || args.length > 2) {
+            System.err.println("Usage: java Program <file_path1> <file_path2>");
             System.exit(-1);
         }
-        // FileWriter
-        // FileReader
-        BufferedReader bufferedReader = null;
-        BufferedReader bufferedReader2 = null;
-        try {
-         bufferedReader = new BufferedReader(new FileReader(args[0]));
-         bufferedReader2 = new BufferedReader(new FileReader(args[1]));
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        
-        }        
-
+        CosineSimilariy cs = new CosineSimilariy(args[0], args[1]);
+        System.out.printf("Similarity = %.2f\n", cs.calcuateCosSim());
     }
+
 }
