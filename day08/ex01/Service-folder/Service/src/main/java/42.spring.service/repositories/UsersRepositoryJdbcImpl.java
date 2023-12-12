9c73526138f9e988fc8267b88ded3377c17948ef
Rida Ennasry <rida.ennasry@gmail.com>
2023-12-12 18:02:16 +0100

    day08/ex01 done

---
+++ day08/ex01/Service-folder/Service/src/main/java/42.spring.service/repositories/UsersRepositoryJdbcImpl.java
@@ -0,0 +1,143 @@
+
+package fortytwo.spring.service.repositories;
+
+import javax.sql.DataSource;
+import fortytwo.spring.service.repositories.UsersRepository;
+import java.util.List;
+import fortytwo.spring.service.models.User;
+import java.util.Optional;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class UsersRepositoryJdbcImpl implements UsersRepository {
+
+    private DataSource ds = null;
+
+    public UsersRepositoryJdbcImpl(DataSource ds) {
+        this.ds = ds;
+    }
+
+    private void deleteAllUser() {
+        Connection connection = null;
+        String sql = "TRUNCATE TABLE users RESTART IDENTITY CASCADE";
+        try (Connection conn = this.ds.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public DataSource getDataSource() {
+        return this.ds;
+    }
+
+    @Override
+    public User findById(Long id) {
+        Connection connection = null;
+        String sql = "SELECT * FROM users WHERE id = ?";
+        User user = null;
+        try (Connection conn = this.ds.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setEmail(resultSet.getString("email"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return user;
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        String sql = "SELECT * FROM users";
+        List<User> users = new ArrayList<>();
+        Connection connection = null;
+        try (Connection conn = this.ds.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setEmail(resultSet.getString("email"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    @Override
+    public void save(User user) {
+        Connection connection = null;
+        String sql = "INSERT INTO users (email) VALUES (?)";
+        try (Connection conn = this.ds.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setString(1, user.getEmail());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(User user) {
+        String sql = "UPDATE users SET email = ? WHERE id = ?";
+        Connection connection = null;
+        try (Connection conn = this.ds.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setString(1, user.getEmail());
+            preparedStatement.setLong(2, user.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        Connection connection = null;
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (Connection conn = this.ds.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        String sql = "SELECT * FROM users WHERE email = ?";
+        Connection connection = null;
+        User user = null;
+        try (Connection conn = this.ds.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setEmail(resultSet.getString("email"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return Optional.ofNullable(user);
+    }
+
+}
---
+++ day08/ex01/Service-folder/Service/src/main/java/42.spring.service/repositories/UsersRepositoryJdbcTemplateImpl.java
@@ -0,0 +1,100 @@
+
+package fortytwo.spring.service.repositories;
+
+import fortytwo.spring.service.repositories.UsersRepositoryJdbcTemplateImpl;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import fortytwo.spring.service.models.User;
+import java.util.Optional;
+import java.util.ArrayList;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+public class UsersRepositoryJdbcTemplateImpl implements UsersRepository {
+    private DataSource ds;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private void removeAllUser() {
+        String sql = "TRUNCATE TABLE users RESTART IDENTITY CASCADE";
+        SqlParameterSource namedParameters = new MapSqlParameterSource();
+        namedParameterJdbcTemplate.update(sql, namedParameters);
+    }
+
+    public UsersRepositoryJdbcTemplateImpl(DataSource ds) {
+        this.ds = ds;
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(ds);
+        // to reset the table
+        this.removeAllUser();
+    }
+
+    public DataSource getDataSource() {
+        return this.ds;
+    }
+
+    @Override
+    public User findById(Long id) {
+        String sql = "SELECT * FROM users WHERE id = :id";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
+        User user = namedParameterJdbcTemplate.queryForObject(sql, namedParameters, (rs, rowNum) -> {
+            User tmpUser = new User();
+            tmpUser.setId(rs.getLong("id"));
+            tmpUser.setEmail(rs.getString("email"));
+            return tmpUser;
+        });
+        return user;
+    }
+
+    @Override
+    public List<User> findAll() {
+        String sql = "SELECT * FROM users";
+        List<User> users = namedParameterJdbcTemplate.query(sql, (rs, rowNum) -> {
+            User tmpUser = new User();
+            tmpUser.setId(rs.getLong("id"));
+            tmpUser.setEmail(rs.getString("email"));
+            return tmpUser;
+        });
+        return users;
+    }
+
+    @Override
+    public void save(User user) {
+        String sql = "INSERT INTO users (email) VALUES (:email)";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("email", user.getEmail());
+        namedParameterJdbcTemplate.update(sql, namedParameters);
+    }
+
+    @Override
+    public void update(User user) {
+        String sql = "UPDATE users SET email = :email WHERE id = :id";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("email", user.getEmail()).addValue("id",
+                user.getId());
+        namedParameterJdbcTemplate.update(sql, namedParameters);
+    }
+
+    @Override
+    public void delete(Long id) {
+        String sql = "DELETE FROM users WHERE id = :id";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
+        namedParameterJdbcTemplate.update(sql, namedParameters);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        String sql = "SELECT * FROM users WHERE email = :email";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("email", email);
+        User user = namedParameterJdbcTemplate.queryForObject(sql, namedParameters, (rs, rowNum) -> {
+            User tmpUser = new User();
+            tmpUser.setId(rs.getLong("id"));
+            tmpUser.setEmail(rs.getString("email"));
+            return tmpUser;
+        });
+
+        return Optional.ofNullable(user);
+    }
+}
